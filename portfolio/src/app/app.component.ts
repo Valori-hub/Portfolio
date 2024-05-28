3832e3422bae55a30382bbe2798ff9de8da2b595
Adam_Elcner <mradam621@gmail.com>
2024-05-28 19:45:01 +0200

    some changes

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,33 @@ import { CommonModule } from '@angular/common';
 export class AppComponent implements OnInit, OnDestroy {
   private scrollListener: (() => void) | undefined;
   title = 'portfolio';
+  lang: string = 'pl'
+  langData = {
+    pl: {
+      header:{
+        home: 'Strona główna',
+        about: 'O mnie',
+        projects: 'Projekty', 
+        contact: 'Kontakt'
+      },
+      sectionHome:{
+        firstLine: 'Cześć, jestem Adam',
+      },
+      sectionAbout:{
+        caption: 'O mnie',
+        desription: 'Nazywam się Adam i mam 22 lata. Jestem aspirującym programistą, który od momentu otrzymania pierwszego komputera z pasją rozwija swoje umiejętności w świecie technologii. Na początku mojej przygody z programowaniem zafascynowało mnie tworzenie gier. Postanowiłem stworzyć przygodową platformówkę z ciekawą fabułą i nietuzinkową grafiką, mimo praktycznie zerowego budżetu. W tym celu rozpocząłem naukę C#, ponieważ pierwszym silnikiem, z jakim się zetknąłem, był Unity. Niestety, z czasem zaczęło brakować czasu i rąk do pracy, zwłaszcza w dziedzinie model designu, więc projekt został porzucony. Moim największym marzeniem jest zdobycie środków na dokończenie tego projektu, który uważam za projekt "życia". Później przerzuciłem się na mniej ambitne projekty, takie jak gry mobilne. W gimnazjum zacząłem interesować się web developingiem i ta pasja trwa do dziś. Obecnie koncentruję się na nauce frameworka Angulara i biblioteki Reacta. Mimo że wiele moich projektów nie ma dokumentacji z powodu zaniedbania prowadzenia GitHuba, staram się to nadrobić i udowodnić, że moją główną motywacją jest pasja.',
+        knowladge: 'Umiejętności'
+      },
+      sectionProjects:{
+        caption: 'Moje projekty',
+        projects:{
+        }
+        }
+    },
+    en: {
 
+    }
+  }
   constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
@@ -45,7 +72,9 @@ export class AppComponent implements OnInit, OnDestroy {
       });
     });
   }
+changelang(){
 
+}
   ngOnDestroy() {
     if (this.scrollListener) {
       this.scrollListener();
